781d7ad9f8d58ed9708496d04ac55ff7fb106bd6
PsSave <pedrosviki1265@gmail.com>
2025-02-09 22:05:05 -0300

    style: enhance header layout and improve button design in Header component

--- components/main/header.tsx
+++ components/main/header.tsx
@@ -1,24 +1,32 @@
-import { Bookmark } from "lucide-react";
+"use client";
+
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { Sun, Moon, Stars, Bookmark } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import { ThemeToggleMenu } from "../theme-menu";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between mb-8 p-4">
-      <h1 className="text-2xl font-bold bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
-        MOVIE AI
-      </h1>
-
-      <div className="flex gap-10">
-        <Link
-          href="/watchlist"
-          className="flex items-center gap-2 text-white/80 hover:text-white"
-        >
-          <Bookmark className="w-4 h-4" />
-          Minha Watchlist
+    <header className="flex items-center justify-between p-4 bg-background text-foreground max-w-screen-xl mx-auto">
+      <div className="text-2xl font-bold">
+        <h1 className="text-2xl font-bold bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
+          MOVIE AI
+        </h1>
+      </div>
+      <div className="flex items-center gap-4">
+        <Link href="/watchlist">
+          <button
+            className="flex items-center gap-2 border-2 border-primary rounded-full px-4 py-2 hover:bg-primary hover:bg-opacity-10 transition-colors
+            hover:text-black"
+          >
+            <Bookmark className="w-6 h-6" />
+            <p className="">Watchlist</p>
+          </button>
         </Link>
         <ThemeToggleMenu />
       </div>
-    </div>
+    </header>
   );
 }
